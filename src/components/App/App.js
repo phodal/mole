5e8bb009233071e5aa679700f38dffea4a0c0626
Fengda HUANG <h@phodal.com>
2016-08-23 07:26:37 +0800

    [clean] remove footer & feedback

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,8 +11,6 @@ import React, { Component, PropTypes } from 'react';
 import emptyFunction from 'fbjs/lib/emptyFunction';
 import s from './App.css';
 import Header from '../Header';
-import Feedback from '../Feedback';
-import Footer from '../Footer';
 
 class App extends Component {
 
@@ -55,8 +53,6 @@ class App extends Component {
       <div>
         <Header />
         {this.props.children}
-        <Feedback />
-        <Footer />
       </div>
     ) : this.props.children;
   }
