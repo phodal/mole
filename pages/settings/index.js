486fb1dce5f8762996c8df90231cf0f6b6aa0b3e
Fengda HUANG <h@phodal.com>
2016-08-24 07:52:13 +0800

    create basic settings

--- pages/settings/index.js
+++ pages/settings/index.js
@@ -6,10 +6,23 @@ import {Textfield} from 'react-mdl/lib';
 class SettingsPage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      GITHUB_REPO: '',
-      GITHUB_TOKEN: ''
-    };
+    var repo = localStorage.getItem('settings.repo');
+    var token = localStorage.getItem('settings.token');
+
+    console.log(repo, token);
+
+    if(repo && token) {
+      this.state = {
+        GITHUB_REPO: repo,
+        GITHUB_TOKEN: token
+      };
+    } else {
+      this.state = {
+        GITHUB_REPO: '',
+        GITHUB_TOKEN: ''
+      };
+    }
+
 
     this.handleRepoChange = this.handleRepoChange.bind(this);
     this.handleTokenChange = this.handleTokenChange.bind(this);
@@ -20,17 +33,19 @@ class SettingsPage extends React.Component {
   }
 
   handleRepoChange(event) {
+    var githubrepo = event.target.value;
     this.setState({
-      GITHUB_REPO: event.target.value
+      GITHUB_REPO: githubrepo
     });
-    console.log(this.state);
+    localStorage.setItem('settings.repo', githubrepo);
   }
 
   handleTokenChange(event) {
+    var githubtoken = event.target.value;
     this.setState({
-      GITHUB_TOKEN: event.target.value
+      GITHUB_TOKEN: githubtoken
     });
-    console.log(this.state);
+    localStorage.setItem('settings.token', githubtoken);
   }
 
   render() {
@@ -38,15 +53,19 @@ class SettingsPage extends React.Component {
       <Layout className={s.content}>
         <Textfield
           onChange={this.handleRepoChange}
-          pattern="((git|ssh|http(s)?)|(git@[\w\.]+))(:(//)?)([\w\.@\:/\-~]+)(\.git)(/)?"
+          pattern="((git|ssh|http(s)?)|(git@[\w\.]+))(:(//)?)([\w\.@\:/\-~]+)?"
           error="Input is not a valid GitHub url!"
           label="GitHub Repo"
           floatingLabel
+          value={this.state.GITHUB_REPO}
+          style={{width: '100%'}}
         />
         <Textfield
           onChange={this.handleTokenChange}
           label="GitHub Token"
           floatingLabel
+          value={this.state.GITHUB_TOKEN}
+          style={{width: '100%'}}
         />
       </Layout>
     );
