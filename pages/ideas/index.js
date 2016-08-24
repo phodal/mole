71f0254638b7adb7fb1b9239a817de643f73124f
Fengda HUANG <h@phodal.com>
2016-08-24 12:44:33 +0800

    create idea lists page

--- pages/ideas/index.js
+++ pages/ideas/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import Layout from '../../components/Layout';
 import s from './styles.css';
+import Button from 'react-mdl/lib/Button';
+import {Card, CardTitle, CardText, CardActions} from 'react-mdl/lib/Card';
+import Link from '../../components/Link';
 
 class IdeasPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ideas: props.data
+    };
+  }
 
   componentDidMount() {
     document.title = 'Ideas';
@@ -11,11 +20,19 @@ class IdeasPage extends React.Component {
   render() {
     return (
       <Layout className={s.content}>
-        <ul>
-          <li>some ideas</li>
-          <li>some ideas</li>
-          <li>some ideas</li>
-        </ul>
+        <div className="note-list">
+          {this.state.ideas.map((idea, i) =>
+            <Card shadow={0} key={i} style={{width: '100%', margin: '0 auto 16px', minHeight: 'auto'}}>
+              <CardTitle>{idea.title}</CardTitle>
+              <CardText>{idea.body}</CardText>
+              <CardActions border>
+                <Button colored>Edit</Button>
+                <Button colored>View</Button>
+                <Button colored>Trash</Button>
+              </CardActions>
+            </Card>
+          )}
+        </div>
       </Layout>
     );
   }
