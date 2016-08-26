2214726bb6ca9c4a540c4b414fb6055e505b64e1
Fengda HUANG <h@phodal.com>
2016-08-26 10:01:58 +0800

    move back redux

--- main.js
+++ main.js
@@ -2,23 +2,28 @@ import "babel-polyfill";
 import "whatwg-fetch";
 import React from "react";
 import {render} from "react-dom";
+import {Provider} from "react-redux";
 import FastClick from "fastclick";
 import {Router, Route, browserHistory} from "react-router";
 import HomePage from "./pages/home";
 import ErrorPage from "./pages/error";
 import IdeasPage from "./pages/ideas";
 import TodoListPage from "./pages/todolist";
+import store from "./core/store";
 
 function startApp() {
   render((
-    <Router history={browserHistory}>
-      <Route path="/" component={HomePage}>
-        <Route path="ideas" component={IdeasPage}/>
-        <Route path="todolist" component={TodoListPage}/>
-        <Route path="*" component={ErrorPage}/>
-      </Route>
-    </Router>
+    <Provider store={store}>
+      <Router history={browserHistory}>
+        <Route path="/" component={HomePage}>
+          <Route path="ideas" component={IdeasPage}/>
+          <Route path="todolist" component={TodoListPage}/>
+          <Route path="*" component={ErrorPage}/>
+        </Route>
+      </Router>
+    </Provider>
   ), document.getElementById('container'));
+
   FastClick.attach(document.body);
 }
 
