252d46f861a325948c6d06937cec1a4357a56f18
Fengda HUANG <h@phodal.com>
2016-08-29 07:32:45 +0800

    inline headers

--- components/NoteLayout/NoteLayout.js
+++ components/NoteLayout/NoteLayout.js
@@ -1,7 +1,7 @@
 import React, {PropTypes} from "react";
 import cx from "classnames";
 import s from "./NoteLayout.css";
-import Header from "./Header";
+import {Link} from "react-router";
 
 class NoteLayout extends React.Component {
 
@@ -21,9 +21,19 @@ class NoteLayout extends React.Component {
     return (
       <div className="mdl-layout mdl-js-layout" ref={node => (this.root = node)}>
         <div className="mdl-layout__inner-container">
-          <Header />
+          <header className={`mdl-layout__header ${s.header}`}>
+            <div className={`mdl-layout__header-row ${s.row}`}>
+              <Link className={`mdl-layout-title ${s.title}`} to="/">
+                <span><i className="fa fa-chevron-left"></i>返回</span>
+              </Link>
+              <div className="mdl-layout-spacer"></div>
+              <nav className="mdl-navigation">
+                <a className="mdl-navigation__link" style={{fontSize: '20px'}}><i className="fa fa-send"></i></a>
+              </nav>
+            </div>
+          </header>
           <main className="mdl-layout__content">
-            <div {...this.props} className={cx(s.content, this.props.className)} />
+            <div {...this.props} className={cx(s.content, this.props.className)}/>
           </main>
         </div>
       </div>
