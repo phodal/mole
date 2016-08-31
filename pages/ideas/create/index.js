f18746ccd61a08de8cd0c537da7cb3d5193685b0
Fengda HUANG <h@phodal.com>
2016-08-31 10:11:00 +0800

    [ideas] create page items

--- pages/ideas/create/index.js
+++ pages/ideas/create/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import NoteLayout from "../../../components/NoteLayout";
+import Textfield from 'react-mdl/lib/Textfield';
 
 class IdeasCreatePage extends React.Component {
   constructor(props) {
@@ -9,7 +10,23 @@ class IdeasCreatePage extends React.Component {
   render() {
     return (
       <NoteLayout rootUrl='/ideas'>
-        Creates
+        <div style={{padding: '20px'}}>
+          <div>
+            <Textfield
+              floatingLabel
+              label="标题..."
+              style={{width: '100%'}}
+            />
+          </div>
+          <div>
+            <Textfield
+              floatingLabel
+              label="内容..."
+              rows={10}
+              style={{width: '100%', height: '100%', display: 'block'}}
+            />
+          </div>
+        </div>
       </NoteLayout>
     )
   }
