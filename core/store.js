612bd7aa064d7c474e6a4a3868143cc98ffec93a
Fengda HUANG <h@phodal.com>
2016-08-31 17:12:45 +0800

    Revert "create reducer;"
    This reverts commit e8f7776d8927353123213b586803401862f4cb3e.

--- core/store.js
+++ core/store.js
@@ -1,14 +1,15 @@
-import { createStore, applyMiddleware } from 'redux';
-
-import apiMiddleware from './middleware/api.js';
-import rootReducer from './reducer';
-
-const createStoreWithMiddleware = applyMiddleware(
-  apiMiddleware
-)(createStore);
+import { createStore } from 'redux';
 
 // Centralized application state
 // For more information visit http://redux.js.org/
-const store = createStoreWithMiddleware(rootReducer, {});
+const store = createStore((state, action) => {
+  // TODO: Add action handlers (aka "reduces")
+  switch (action) {
+    case 'COUNT':
+      return { ...state, count: (state.count || 0) + 1 };
+    default:
+      return state;
+  }
+});
 
 export default store;
